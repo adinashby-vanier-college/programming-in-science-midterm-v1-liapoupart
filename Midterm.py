1713d620cdfd3cd348ba238eb34f20906325d0e6
Lia Poupart <2470667@edu.vaniercollege.qc.ca>
2025-10-21 09:41:03 -0400

    midterm

--- Midterm.py
+++ Midterm.py
@@ -2,15 +2,36 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    area = math.pi * radius ** 2
+    return round(area, 2)
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    if n<4:
+        return "The triangle height should be at least 4."
+    else:
+        for i in range(1, n+1):
+            if i == 1:
+                line = "*" 
+            elif i == i:
+                line = ("*") * (n)
+            else:
+                line = "*" + (i-2)*(" ") + "*" 
+        return line.rstrip
+# To make a hollow triangle, I already know that it starts with a * at the start of each line but the nb of spaces is dependant on i.
+# There are two * per line except line 1 and line n, where there are only 1 * at n=1 an n starts and line n
+# With a if else code, i code the exceptions and else is everything that isn't i =1 or n. 
+# There are 
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    if n<3:
+        return "The pyramid height should be at least 3."
+    else:
+        for i in range(1, n+1):
+            line = (" " * (i-1)) + (("*") * ((2*n+1)-2*i)) + "\n"
+        return str(line)
+    
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
